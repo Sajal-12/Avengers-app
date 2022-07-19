21c0f0b2249e1da1a925d4be942be2865ca399cd
Sajal Swapnil <sajalvictorious83@gmail.com>
2022-07-19 16:42:03 +0530

    INITIAL ADD

---
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.activitylifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println("onCreate Called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("onStart Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("onResume Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("onStop Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("onRestart Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("onDestroy Called")
+    }
+}
